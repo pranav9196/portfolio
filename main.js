bf4092f47647223d5d641fef016973e38e685b0a
Pranav <pranav9196@gmail.com>
2017-06-26 12:50:09 +0530

    navigation hover state

--- main.js
+++ main.js
@@ -1,6 +1,117 @@
-var navigationCircles= getElementByClassName("fa-circle-o");
+$(document).ready(function () {
 
-navigationCircles.onmouseover= function(){
-	var toggle=document.getElementById("toggle1");
-	TweenLite.to(toggle, 1, {left:"-50px"});
-}
+    $('.right_nav').mouseover(function (e) {
+        var id = e.target.id;
+        if(id == ''){
+        	var icon = $(this).find('span');
+        	TweenLite.to(icon, 0.3, { left:"-50px", opacity: "1" });
+        }
+    });
+
+
+    $('.right_nav').mouseleave(function (e) {
+        var id = e.target.id;
+        if(id == ''){
+        	var icon = $(this).find('span');
+        	TweenLite.to(icon, 0.3, { opacity: "0", left:"0px"  });
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+// $(window).scroll(function(){
+// 	console.log("scroll");
+// 		var scroll=$(window).scrollTop();
+// 		if(scroll>=100){
+// 			$('section').each(sectionIdentification());
+// 		}
+// 	});
+
+// 	var sectionIdentification= function(){
+// 		if($(this).position().top<=scroll){
+// 			getNavigationElement();
+// 		}
+// 	}
+
+// 	var getNavigationElement= function(){
+// 		var navigation=document.getElementsById('right_nav');
+// 		navigation.each(navigationLoop());
+// 	}
+
+// 	var navigationLoop= function(){
+// 		var anchors= navigation.getElementsByClassName('fa-circle-o');
+// 		anchors.each(function(){
+// 			if (($('section').id=='home')) {
+// 				$("#toggle").toggleClass('fa-circle');
+// 			}
+
+// 			if (($('section').id=='port')) {
+// 				$("#toggle1").toggleClass('fa-circle');
+// 			}
+
+// 			if (($('section').id=='contact')) {
+// 				$("#toggle2").toggleClass('fa-circle');
+// 			}
+
+// 			if (($('section').id=='about')) {
+// 				$("#toggle3").toggleClass('fa-circle');
+// 			}
+// 		});
+// 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// $(window).scroll(function() {
+	// 	var windscroll = $(window).scrollTop();
+	// 	if (windscroll >= 100) {
+			
+	// 		$('section').each(function(i) {
+	// 			if ( $(this).position().top <= windscroll){
+	// 				$('nav a.active').removeClass('active');
+	// 				$('nav a').eq(i).addClass('active');
+	// 				$("#").toggleClass('change_me newClass');
+				// }
+			// });
+
+		// } else {
+
+			// $('nav').removeClass('color');
+			// $('nav a.active').removeClass('active');
+			// $('nav a:first').addClass('active');
+		// }
+
+	// }).scroll();
+
+	// changing color of left nav
+	// $(document).on('scroll', function() {
+	// 	if($(this).scrollTop()>=$('contact','port').position().top){			
+	// 			$(".nav").css("color", "white");			
+	// 	}
+	// })
+
+	// var element= document.getElementsByClassName("right_nav");
+	// console.log(element);
